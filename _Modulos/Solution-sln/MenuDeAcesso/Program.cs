5e9ec7aeb4097bbe4bb813d10e1c172836519897
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-01-10 23:09:19 -0300

    ---Inicio de solution em c#

---
+++ _Modulos/Solution-sln/MenuDeAcesso/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string? opcaoSelecionada = "null";
+bool opcaoValida = true;
+do
+{
+    Console.WriteLine("-------Login------\n"+
+                      "[1] Cliente\n"+
+                      "[2] Adimin");
+    opcaoSelecionada = Console.ReadLine();
+    if (opcaoSelecionada == "1" || opcaoSelecionada == "2")
+    {
+        opcaoValida = true;
+    } else
+    {
+        opcaoValida = false;
+        Console.WriteLine("\tOpção não encontrada, favor informe uma opção valida...");
+    }
+} while (!opcaoValida);
