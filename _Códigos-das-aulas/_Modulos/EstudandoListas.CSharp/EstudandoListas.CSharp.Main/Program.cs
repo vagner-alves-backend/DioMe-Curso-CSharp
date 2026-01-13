3dd59e543d8dd4e9c72b44b3bceef4707c5cc000
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-01-12 22:54:00 -0300

    ---Lista em csharp

---
+++ _Códigos-das-aulas/_Modulos/EstudandoListas.CSharp/EstudandoListas.CSharp.Main/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> listNames = [];
+listNames.Add("Vágner");
+listNames.Add("Lucas");
+listNames.Add("Fernanda");
+listNames.Add("Sara");
+
+listNames.AddRange("Matheus", "Gustavo", "Laura", "Mariana");
+
+foreach (string nome in listNames)
+{
+    Console.WriteLine(nome);
+}
+
+
+// string[] names = ["Vágner", "Lucas", "Marciel"];
+// Array.Resize(ref names, names.Length * 2);
+// Console.WriteLine(names.Length);
