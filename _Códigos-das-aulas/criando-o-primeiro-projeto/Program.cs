5a689f78280c4d763792c760855a10e4cacfefd1
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-01-24 15:26:32 -0300

    Apaguei pasta de projeto vazia.

--- _Códigos-das-aulas/criando-o-primeiro-projeto/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Olá, mundo! Esse é meu primeiro comando em C# -- Inicio dos meus estudos com a dio.me...");
-Console.WriteLine("TESTE -- GitHub");
